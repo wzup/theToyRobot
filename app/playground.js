53f9feca7a1fbfb80888212f0d08712866c9e0a5
wzup <weiss10700@gmail.com>
2015-09-29 22:31:04 +0800

    Clean app/playground.js (remove all comments)

--- app/playground.js
+++ app/playground.js
@@ -23,34 +23,3 @@ var prototype = {
 
 Playgroud.prototype = Object.create(prototype);
 Playgroud.prototype.constructor = Playgroud;
-
-/**
- * Класс "Игровое поле"
- * config = {x: 5, y: 5}
- */
-// function Playgroud(config) {
-
-//     var config = config || {};
-
-//     // если нет в конфиге X или Y, ставим по умолчанию
-//     var oStartPoint = {
-//         x: config.startPointX || 0,
-//         y: config.startPointY || 0
-//     };
-//     var oLength = {
-//         x: config.lengthX || 5,
-//         y: config.lengthY || 5
-//     };
-
-//     this.isEndOfPlayground = function(x, y) {
-//         if (
-//             (x > (oStartPoint.x + (oLength.x - 1))) ||
-//             (x < oStartPoint.x) ||
-//             (y > (oStartPoint.y + (oLength.y - 1))) ||
-//             (y < oStartPoint.y)
-//         ) {
-//             return true;
-//         } else
-//             return true;
-//     };
-// }

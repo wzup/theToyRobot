48504750d857e77f942b5450fc6ebf8cef58d88a
wzup <weiss10700@gmail.com>
2015-10-14 01:26:46 +0800

    Adjustment in package.json & toyRobotApp.js - comments & install 'readline' package

--- app/toyRobotApp.js
+++ app/toyRobotApp.js
@@ -1,47 +1,41 @@
 'use strict';
 
 /**
- * "The Toy Robot" app.
- * It is a mudule.
+ * The Toy Robot Simulator app. It is a module. The main goal of this module is
+ * to start an app. It gets the robot instance and uses its methods to controll
+ * the robot and output messages to user.
+ */
+
+/**
+ * Declare and initialize variables
  */
 var os = require("os"), // to have platform independent EOL
     stdin = process.stdin,
     stdout = process.stdout,
     stderr = process.stderr,
     robot = require('./robotFactory'), // a robot instance
-    EOL = os.EOL,
+    EOL = os.EOL, // cross-platform EOL
     fs = require('fs'), // to check if a file exists and is readable and to create a stream
-    readline = require('readline'), // to read commands from a file
-    rl,
+    readline = require('readline'), // Readline class. To read commands from a file
+    rl, // readline instance
     argv, // for cli arguments, particularly to get a file path
     messenger = robot.getMessenger(); // to create and send messages to user
 
 stdin.setEncoding('utf8');
 process.title = "== The Toy Robot =="; // sets a terminal title
 
-argv = process.argv.slice(2);
+argv = process.argv.slice(2); // get only the name of the file from user prompt
 
 
+// read stdin
+// this piece of code is for reading user's input from CLI
 stdin.on('data', function(data) {
-    // var res, _data = data.trim();
-    // if (_data.match(/(q|quit|exit)/i))
-    //     process.exit();
-
-    // res = doAction(_data);
-    // if (res instanceof Error) {
-    //     stderr.write(res.message + EOL + '> ');
-    // } else if (typeof res == 'string') {
-    //     stdout.write(res + EOL + '> ');
-    // } else {
-    //     stdout.write('> ');
-    // }
     doOutput(data);
 });
 
 
+// this piece of code is for reading commands from a file
 if (argv.length) {
-    stdout.write("ARGV: " + argv + EOL);
-
     try {
         fs.accessSync(argv[0], fs.F_OK | fs.R_OK)
     } catch (e) {
@@ -58,32 +52,30 @@ if (argv.length) {
         terminal: false
     });
 
+    // event handler. is called when a line is read from a file
     rl.on('line', function(line) {
         stdout.write(line + EOL);
-
-        // var res, _line = line.trim();
-        // res = doAction(_line);
-        // if (res instanceof Error) {
-        //     stderr.write(res.message + EOL + '> ');
-        // } else if (typeof res == 'string') {
-        //     stdout.write(res + EOL + '> ');
-        // } else {
-        //     stdout.write('> ');
-        // }
-
         doOutput(line);
-
-        // stdout.write(doAction(line) + EOL);
     });
 
+    // event handler. is called when all the lines in a file have been read
+    // closes a stream and exit
     rl.on('close', function() {
-        console.log('== END ==: ');
         rl.close();
         process.exit();
     });
 }
 
 
+/**
+ * This parser encapsulates the task of reading a user's input, either form CLI
+ * or from a file.
+ * @param  {String} sCommand a command from a user, lile "PLACE, MOVE, etc."
+ * @return {Error|String|Object} returns either an Error instance, or a message
+ * string, or robot instance. The return value depends on what the robot
+ * returned. A successful action returns robot's instance.
+ * @private
+ */
 function doAction(sCommand) {
     var res;
     // PLACE X(,| )Y(,| )F(  *)
@@ -106,6 +98,12 @@ function doAction(sCommand) {
     return res;
 }
 
+/**
+ * Sends a response from doAction() to stdout or stderr
+ * @param  {Error|String|Object} either an Error instance, or a message string,
+ * or robot instance.
+ * @return {undefined}      no return. the func only sends to stdout or stderr
+ */
 function doOutput(data) {
     var res, _data = data.trim();
 
@@ -122,7 +120,16 @@ function doOutput(data) {
     }
 }
 
+
+/**
+ * A class TheToyRobotApp
+ * It has only one static method .run() to strat the app
+ */
 function TheToyRobotApp() {};
+
+/**
+ * @static
+ */
 TheToyRobotApp.run = function() {
     stdout.write(messenger.getMessage({
         msg: 'welcome',
